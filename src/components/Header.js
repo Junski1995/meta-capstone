74205683e2f3faf7261448771b229d6d34905703
Junski1995 <Junsik1995@gmail.com>
2024-05-23 09:32:53 -0700

    last change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import restaurantfood from '../Assets/restauranfood.jpg';
+import restaurantfood from '../images/restauranfood.jpg';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
